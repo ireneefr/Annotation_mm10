842a8c145984a9a0c0c6feafb02183843b473548
ireneefr <irene.f.r.2000@gmail.com>
2022-07-04 10:45:34 +0200

    Generate table annotation for transcripts

--- annotation_mm10.R
+++ annotation_mm10.R
@@ -16,19 +16,23 @@ download.file(url, destfile)
 
 data <- read.table(gzfile("~/Annotation_mm10/knownGene.txt.gz"), sep = "\t")
 head(data)
+
 # Assign colnames based on knownGene.sql file
 cols <- c("name", "chrom", "strand", "txStart", "txEnd", "cdsStart", "cdsEnd",
           "exonCount", "exonStarts", "exonEnds", "proteinID", "alignID")
 colnames(data) <- cols
 dim(data) #142446     12
 length(unique(data$proteinID)) #53246
-length(unique(sub("\\..*", "", data$name))) 
-head(data, 20)
+length(unique(sub("\\..*", "", data$name))) #142446
+data$id_transcript <- sub("\\..*", "", data$name)
+
 # Generate exon start and end columns for 1st exon
 data[data$strand == "+", "start_1stExon"] <- vapply(strsplit(data[data$strand == "+", "exonStarts"],","), head, 1, FUN.VALUE=character(1))
 data[data$strand == "-", "start_1stExon"] <- vapply(strsplit(data[data$strand == "-", "exonStarts"],","), tail, 1, FUN.VALUE=character(1))
 data[data$strand == "+", "end_1stExon"] <- vapply(strsplit(data[data$strand == "+", "exonEnds"],","), head, 1, FUN.VALUE=character(1))
 data[data$strand == "-", "end_1stExon"] <- vapply(strsplit(data[data$strand == "-", "exonEnds"],","), tail, 1, FUN.VALUE=character(1))
+head(data, 10)
+
 
 ### Obtain gene names
 
@@ -37,10 +41,17 @@ library(biomaRt)
 mart <- useMart("ensembl", "mmusculus_gene_ensembl")
 ensemble2gene <- getBM(attributes = c("ensembl_transcript_id", "external_gene_name", "ensembl_gene_id"),
                        filters = "ensembl_transcript_id",
-                       values = sub("\\..*", "", data$name), mart = mart)
+                       values = data$id_transcript, mart = mart)
 head(ensemble2gene)
 dim(ensemble2gene) #139239      3
 length(unique(ensemble2gene$ensembl_gene_id)) #52564
 ensemble2gene[ensemble2gene$external_gene_name == "Ptpru",]
 
+# Add gene name to principal table annotation
+table <- merge(data, ensemble2gene, by.x = "id_transcript", by.y = "ensembl_transcript_id")
+head(table)
+dim(table) #139239     17
 
+
+### Save table annotation
+write.csv(table, "annotation_transcript.csv", row.names = FALSE)
